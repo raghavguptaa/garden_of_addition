00a58ea3ab2ab93694debb03f3ae3e1ac2859d75
Raghav Gupta <66035668+raghavguptaa@users.noreply.github.com>
2021-10-21 22:47:33 +0530

    Update processing.js

--- processing.js
+++ processing.js
@@ -1,6 +1,7 @@
 
 var model;
 
+
 async function loadModel() {
     model = await tf.loadGraphModel('TFJS/model.json')
 }
@@ -8,19 +9,29 @@ async function loadModel() {
 function predictImage() {
     // console.log('processing ...');
 
+    //load the image!!
     let image = cv.imread(canvas);
+
+    //change the colur to single colour i.e. GRAYSCALE
     cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
+
+    //change the colour to white for presentation
     cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
 
+    //finding Contours
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     // You can try more different parameters
     cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
 
+
+    //roi = region of interest - Cropping Image into rectangle
     let cnt = contours.get(0);
     let rect = cv.boundingRect(cnt);
     image = image.roi(rect);
 
+
+    
     var height = image.rows;
     var width = image.cols;
 
@@ -34,14 +45,21 @@ function predictImage() {
         height = Math.round(image.rows / scaleFactor);
     }
 
+
+    //calculate new size of the image
     let newSize = new cv.Size(width, height);
+
+    //resize the image
     cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA)
 
+    // add padding to the image
     const LEFT = Math.ceil(4 + (20 - width) / 2);
     const RIGHT = Math.floor(4 + (20 - width) / 2);
     const TOP = Math.ceil(4 + (20 - height) / 2);
     const BOTTOM = Math.floor(4 + (20 - height) / 2);
     // console.log(`top: ${TOP}, bottom: ${BOTTOM}, left: ${LEFT}, right: ${RIGHT}`);
+
+
 
     const BLACK = new cv.Scalar(0, 0, 0, 0);
     cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK);
@@ -55,6 +73,7 @@ function predictImage() {
     const cy = Moments.m01 / Moments.m00;
     // console.log(`M00: ${Moments.m00}, cx: ${cx}, cy: ${cy}`);
 
+    //shifting the image to the center
     const X_SHIFT = Math.round(image.cols / 2.0 - cx);
     const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
 
@@ -67,18 +86,23 @@ function predictImage() {
 
     pixelValues = Float32Array.from(pixelValues);
 
+    // normalise the image 
     pixelValues = pixelValues.map(function (item) {
         return item / 255.0;
     });
 
     // console.log(`scaled array: ${pixelValues}`);
 
+    
 
+    //create a tensor 
     const X = tf.tensor([pixelValues]);
 
     // console.log(`Shape of Tensor: ${X.shape}`);
     // console.log(`dtype of Tensor: ${X.dtype}`);
 
+
+    // result = the predicted image
     const result = model.predict(X);
     result.print();
     const output = result.dataSync()[0];
@@ -86,7 +110,7 @@ function predictImage() {
 
     // console.log(tf.memory());
     
-    // Testing Only (delete later)
+    // Testing Only
     // const outputCanvas = document.createElement('CANVAS');
     // cv.imshow(outputCanvas, image);
     // document.body.appendChild(outputCanvas);
